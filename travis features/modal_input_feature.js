5281da63efa265ce2a8ef10c34c4984ad16369f4
Travis Ueki <travisu@travispc.none>
2015-06-03 20:44:52 -0700

    pulled todo_list object from server

--- travis features/modal_input_feature.js
+++ travis features/modal_input_feature.js
@@ -2,12 +2,15 @@ function pull_user_todo(){
     $.ajax({
         url: 'http://s-apis.learningfuze.com/todo/get',
         dataType:'JSON',
+        data: {
+        	id:user_object.id
+        },
         cache: false,
-        crossDomain: false,
+        crossDomain: true,
         method:'POST',
         success:function(response){
         	console.log('response is ', response);
-        	userId:  
+
         }
-    })
+    });
 }
