aa13b4eb9aad8555dd3983b3621f0b01a48e4fd3
Jordan <jhealth7@gmail.com>
2015-06-04 17:27:47 -0700

    update

--- main.js
+++ main.js
@@ -174,9 +174,9 @@ function logout() {
 //creates list from todo_array
 function create_list(array) {
     for (var i = 0; i < array.length; i++) {
-        var title = $('<ul>').text(array[i].title);
-        var details = $('<li>').text(array[i].details);
-        var timestamp = $('<li>').text(array[i].timeStamp);
+        var title = $('<ul>').addClass('titled').text(array[i].title);
+        var details = $('<li>').addClass('details').text(array[i].details);
+        var timestamp = $('<li>').addClass('timestamp').text(array[i].timeStamp);
         // var user_id = array[i].user_id;
         var deleteBtn = $('<button>', {
             id: array[i].id,
@@ -213,9 +213,6 @@ function server_call() {
                     todo_array.push(todo_list[i]);
                 }
                 create_list(todo_array);
-            }
-            else{
-                alert('No user info found!');
             }
         }
     });
